8753c061c347b58f5d918c1b010306716451cbe5
multialfredo14 <multialfredo14@gmail.com>
2026-04-27 13:27:31 -0600

    fix: redirect root URL to dashboard

--- vetclinic/urls.py
+++ vetclinic/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 from accounts.views import dashboard
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/dashboard/", permanent=False)),
     path("admin/", admin.site.urls),
     path("dashboard/", dashboard, name="dashboard"),
     path("accounts/", include("accounts.urls")),
